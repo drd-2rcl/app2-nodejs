777a1ee2d77bb030e608d467a0a3327b811f144c
drd-2rcl <drd.rclrd@gmail.com>
2019-07-02 22:58:08 -0300

    :recycle: refactoring

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,9 @@
-const { Router } = require('express');
+import { Router } from 'express';
 
 const routes = new Router();
 
 routes.get('/', (req, res) => {
-  return res.json({ message: 'Hello dude' })
+  return res.json({ message: 'Hello mama' })
 })
 
-module.exports = routes;
+export default routes; 
