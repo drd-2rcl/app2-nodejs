943cba1904cf753412924a9375e0b3b31aa82cc8
Diogo <drd.rclrd@gmail.com>
2019-08-22 23:08:25 -0300

    created data in route for testing ok

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,16 @@
 import { Router } from 'express';
+import User from './app/models/User';
 
 const routes = new Router();
 
-routes.get('/', (req, res) => {
-  return res.json({ message: 'Hello mama' });
+routes.get('/', async (req, res) => {
+  const user = await User.create({
+    name: 'Diogo Dias',
+    email: 'diogo@gmail.com.br',
+    password_hash: 'dfgh3456dfgh&$$#',
+  });
+
+  return res.json(user);
 });
 
 export default routes;
