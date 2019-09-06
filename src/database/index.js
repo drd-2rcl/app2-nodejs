e218cb5ebf7364efdf6be3b8e1a7bb45ad8c67de
Diogo <drd.rclrd@gmail.com>
2019-09-06 10:08:46 -0300

    added appointment in database

--- src/database/index.js
+++ src/database/index.js
@@ -1,10 +1,11 @@
 import Sequelize from 'sequelize';
-import databaseConfig from '../config/database';
 
+import databaseConfig from '../config/database';
 import Users from '../app/models/User';
 import File from '../app/models/File';
+import Appointment from '../app/models/Appointment';
 
-const models = [Users, File];
+const models = [Users, File, Appointment];
 
 class Database {
   constructor() {
