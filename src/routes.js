7fb1ec3da93039dcf30436a6d635b9ebe895cf13
Diogo <drd.rclrd@gmail.com>
2019-09-07 15:40:22 -0300

    added route for create appointment

--- src/routes.js
+++ src/routes.js
@@ -7,19 +7,17 @@ import SessionController from './app/controllers/SessionController';
 import authMiddleware from './app/middleware/auth';
 import FileController from './app/controllers/FileController';
 import ProviderController from './app/controllers/ProviderController';
+import AppointmentController from './app/controllers/AppointmentController';
 
 const routes = new Router();
 const upload = multer(multerConfig);
 
 routes.post('/users', UserController.store);
 routes.post('/sessions', SessionController.store);
-
 routes.use(authMiddleware);
-
 routes.put('/users', UserController.update);
-
 routes.get('/providers', ProviderController.index);
-
+routes.post('/appointments', AppointmentController.store);
 routes.post('/files', upload.single('file'), FileController.store);
 
 export default routes;
