68d7dd25b557ce6db6883387fd4430735c3c6926
Diogo <drd.rclrd@gmail.com>
2019-09-09 15:48:52 -0300

    new metho for update notification to read and new is true

--- src/app/controllers/NotificationController.js
+++ src/app/controllers/NotificationController.js
@@ -20,7 +20,18 @@ class NotificationController {
       .limit(20);
 
     return res.json(notifications);
-    // return res.json();
+  }
+
+  async update(req, res) {
+    // const notification = await Notification.findById(req.params.id);
+
+    const notification = await Notification.findByIdAndUpdate(
+      req.params.id,
+      { read: true },
+      { new: true }
+    );
+
+    return res.json(notification);
   }
 }
 
