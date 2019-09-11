61adc5bf0451636d610cb473a63a64fa68dc0694
Diogo <drd.rclrd@gmail.com>
2019-09-10 23:43:07 -0300

    created route for delete appointment

--- src/routes.js
+++ src/routes.js
@@ -16,14 +16,22 @@ const upload = multer(multerConfig);
 
 routes.post('/users', UserController.store);
 routes.post('/sessions', SessionController.store);
+
 routes.use(authMiddleware);
+
 routes.put('/users', UserController.update);
+
 routes.get('/providers', ProviderController.index);
+
 routes.get('/appointments', AppointmentController.index);
 routes.post('/appointments', AppointmentController.store);
+routes.delete('/appointments/:id', AppointmentController.delete);
+
 routes.get('/schedule', ScheduleController.index);
+
 routes.get('/notifications', NotificationController.index);
 routes.put('/notifications/:id', NotificationController.update);
+
 routes.post('/files', upload.single('file'), FileController.store);
 
 export default routes;
