ba590720577909ce8fda00f15049e08a8b44a1e0
Diogo <drd.rclrd@gmail.com>
2019-09-11 23:23:04 -0300

    created config for connect in service mailer

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  secure: false,
+  auth: {
+    user: '95466c0903beab',
+    pass: 'e7404ac113b61b',
+  },
+  default: {
+    from: 'Equipe Diogo <noreplay@gobarber.com',
+  },
+};
