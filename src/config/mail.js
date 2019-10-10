736dbfeed6e63a795e9b3b173ba709a752f1e43e
Diogo <drd.rclrd@gmail.com>
2019-10-09 21:51:43 -0300

    added variable for mail service

--- src/config/mail.js
+++ src/config/mail.js
@@ -1,10 +1,10 @@
 export default {
-  host: 'smtp.mailtrap.io',
-  port: 2525,
+  host: process.env.MAIL_HOST,
+  port: process.env.MAIL_PORT,
   secure: false,
   auth: {
-    user: '95466c0903beab',
-    pass: 'e7404ac113b61b',
+    user: process.env.MAIL_USER,
+    pass: process.env.MAIL_PASS,
   },
   default: {
     from: 'Equipe Diogo <noreplay@gobarber.com',
